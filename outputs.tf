4dc99f5b4e30a7c5a5a2a98fe61041b427742b25
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,28 @@ output "healthbots" {
   description = "All healthbot resources"
   value       = azurerm_healthbot.healthbots
 }
+output "healthbots_bot_management_portal_url" {
+  description = "List of bot_management_portal_url values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.bot_management_portal_url]
+}
+output "healthbots_location" {
+  description = "List of location values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.location]
+}
+output "healthbots_name" {
+  description = "List of name values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.name]
+}
+output "healthbots_resource_group_name" {
+  description = "List of resource_group_name values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.resource_group_name]
+}
+output "healthbots_sku_name" {
+  description = "List of sku_name values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.sku_name]
+}
+output "healthbots_tags" {
+  description = "List of tags values across all healthbots"
+  value       = [for k, v in azurerm_healthbot.healthbots : v.tags]
+}
 

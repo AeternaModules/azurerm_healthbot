5bb172dd6d6011b69db398684ec636c8c4540b7a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "healthbots" {
-  description = "All healthbot resources"
-  value       = azurerm_healthbot.healthbots
-}
 output "healthbots_bot_management_portal_url" {
-  description = "List of bot_management_portal_url values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.bot_management_portal_url]
+  description = "Map of bot_management_portal_url values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.bot_management_portal_url }
 }
 output "healthbots_location" {
-  description = "List of location values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.location]
+  description = "Map of location values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.location }
 }
 output "healthbots_name" {
-  description = "List of name values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.name]
+  description = "Map of name values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.name }
 }
 output "healthbots_resource_group_name" {
-  description = "List of resource_group_name values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.resource_group_name]
+  description = "Map of resource_group_name values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.resource_group_name }
 }
 output "healthbots_sku_name" {
-  description = "List of sku_name values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.sku_name]
+  description = "Map of sku_name values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.sku_name }
 }
 output "healthbots_tags" {
-  description = "List of tags values across all healthbots"
-  value       = [for k, v in azurerm_healthbot.healthbots : v.tags]
+  description = "Map of tags values across all healthbots, keyed the same as var.healthbots"
+  value       = { for k, v in azurerm_healthbot.healthbots : k => v.tags }
 }
 
